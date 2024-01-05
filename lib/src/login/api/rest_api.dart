9f10147bccca03ca33b81c24006c8cf59a3ef897
Graham Williams <graham.williams@togaware.com>
2024-01-05 19:05:25 +1100

    Restore missing files.

---
+++ lib/src/login/api/rest_api.dart
@@ -0,0 +1,66 @@
+/// Functions with restful APIs.
+///
+// Time-stamp: <Tuesday 2024-01-02 15:57:15 +1100 Zheyuan Xu>
+///
+/// Copyright (C) 2024, Software Innovation Institute, ANU.
+///
+/// Licensed under the MIT License (the "License").
+///
+/// License: https://choosealicense.com/licenses/mit/.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+///
+/// Authors: Zheyuan Xu
+
+library;
+
+import 'package:http/http.dart' as http;
+
+/// The fetchPrvFile function is an asynchronous function designed to fetch
+/// profile data from a specified URL [profCardUrl].
+/// It takes three parameters: [profCardUrl] (the URL to fetch data from),
+/// [accessToken] (used for authorization), and [dPopToken] (another form
+/// of token used in headers for enhanced security).
+
+Future<String> fetchPrvFile(
+  String profCardUrl,
+  String accessToken,
+  String dPopToken,
+) async {
+  final profResponse = await http.get(
+    Uri.parse(profCardUrl),
+    headers: <String, String>{
+      'Accept': '*/*',
+      'Authorization': 'DPoP $accessToken',
+      'Connection': 'keep-alive',
+      'DPoP': dPopToken,
+    },
+  );
+
+  if (profResponse.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return profResponse.body;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    //print(profResponse.body);
+    throw Exception('Failed to load profile data! Try again in a while.');
+  }
+}
---
+++ lib/src/login/solid_authenticate.dart
@@ -0,0 +1,85 @@
+/// Solid authenticate function, return null if authenticate function fails.
+///
+// Time-stamp: <Wednesday 2024-01-03 10:57:15 +1100 Zheyuan Xu>
+///
+/// Copyright (C) 2024, Software Innovation Institute, ANU.
+///
+/// Licensed under the MIT License (the "License").
+///
+/// License: https://choosealicense.com/licenses/mit/.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+///
+/// Authors: Zheyuan Xu
+library;
+
+import 'package:fast_rsa/fast_rsa.dart';
+import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:solid/src/login/api/rest_api.dart';
+import 'package:solid_auth/solid_auth.dart';
+
+// Scopes variables used in the authentication process.
+
+final List<String> _scopes = <String>[
+  'openid',
+  'profile',
+  'offline_access',
+];
+
+/// An asynchronous function designed to authenticate a user
+/// against a Solid server.
+/// [serverId] is an issuer URI and is essential for the
+/// authentication process with the POD (Personal Online Datastore) issuer.
+/// [context] is used in the authenticate method.
+/// The authentication process requires the context of the current widget.
+///
+/// The function returns a list containing authentication data, the user's webId,
+/// and their profile data.
+/// Error Handling: The function has a broad error handling mechanism (on ()), which returns null
+/// if any exception occurs during the authentication process.
+
+Future<List<dynamic>?> solidAuthenticate(
+    String serverId, BuildContext context) async {
+  try {
+    final issuerUri = await getIssuer(serverId);
+
+    // Authentication process for the POD issuer.
+
+    // ignore: use_build_context_synchronously
+    final authData = await authenticate(Uri.parse(issuerUri), _scopes, context);
+
+    final accessToken = authData['accessToken'].toString();
+    final decodedToken = JwtDecoder.decode(accessToken);
+    final webId = decodedToken['webid'].toString();
+
+    final rsaInfo = authData['rsaInfo'];
+    final rsaKeyPair = rsaInfo['rsa'];
+    final publicKeyJwk = rsaInfo['pubKeyJwk'];
+    final profCardUrl = webId.replaceAll('#me', '');
+    final dPopToken =
+        genDpopToken(profCardUrl, rsaKeyPair as KeyPair, publicKeyJwk, 'GET');
+
+    final profData = await fetchPrvFile(profCardUrl, accessToken, dPopToken);
+
+    return [authData, webId, profData];
+  } on () {
+    return null;
+  }
+}
