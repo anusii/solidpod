6b12d49d73eaaf9bb68be7771650e5ae08e7aa1d
cdawei <dawei.chen@anu.edu.au>
2024-05-30 14:01:21 +1000

    Press Enter to submit the security key

--- lib/src/widgets/security_key_input.dart
+++ lib/src/widgets/security_key_input.dart
@@ -29,6 +29,7 @@
 library;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show LogicalKeyboardKey;
 
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
@@ -46,11 +47,12 @@ class SecurityKeyInput extends StatefulWidget {
 
   /// The verification function
   final bool Function(String) verifySecurityKeyFunc;
+
+  /// The child widget
   final Widget child;
 
   @override
-  // ignore: library_private_types_in_public_api
-  _SecurityKeyInputState createState() => _SecurityKeyInputState();
+  State<SecurityKeyInput> createState() => _SecurityKeyInputState();
 }
 
 class _SecurityKeyInputState extends State<SecurityKeyInput> {
@@ -62,134 +64,159 @@ class _SecurityKeyInputState extends State<SecurityKeyInput> {
     const inputKey = 'SecurityKey';
     const message = 'Please enter the security key'
         ' you previously provided to secure your data.';
+
+    // If the security key entered is verfied
+
     var keyVerified = false;
+
+    // Small vertical and horizontal gaps
+
+    const smallGapV = SizedBox(height: 10);
+    const smallGapH = SizedBox(width: 10);
+
+    // The form text field
+
+    final formTextField = FormBuilderTextField(
+      name: inputKey,
+      obscureText:
+          !_showKey, // Controls whether the security key is shown or hidden.
+      autocorrect: false,
+      decoration: InputDecoration(
+        labelText: 'SECURITY KEY',
+        labelStyle: const TextStyle(
+          color: Colors.blue,
+          letterSpacing: 1.5,
+          fontSize: 13.0,
+          fontWeight: FontWeight.bold,
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(_showKey ? Icons.visibility : Icons.visibility_off),
+          onPressed: () {
+            setState(() {
+              // Toggle the state to show/hide the security key.
+              _showKey = !_showKey;
+            });
+          },
+        ),
+      ),
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+        (val) {
+          if (!widget.verifySecurityKeyFunc(val as String)) {
+            keyVerified = false;
+            debugPrint('keyVerified: $keyVerified');
+            return 'Incorrect Security Key';
+          } else {
+            keyVerified = true;
+            debugPrint('keyVerified: $keyVerified');
+          }
+          return null;
+        },
+      ]),
+    );
+
+    // The form builder
+
+    final formBuilder = FormBuilder(
+        key: formKey,
+        onChanged: () {
+          formKey.currentState!.save();
+        },
+        autovalidateMode: AutovalidateMode.always,
+        child: KeyboardListener(
+          focusNode: FocusNode(),
+          onKeyEvent: (event) async {
+            if (event.logicalKey == LogicalKeyboardKey.enter) {
+              await _saveKey(context, formKey, inputKey, keyVerified);
+            }
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _createText('Security Key', fontSize: 20),
+              const Divider(color: Colors.grey),
+              _createText(message, fontSize: 15),
+              smallGapV,
+              StatefulBuilder(
+                builder: (context, setState) => formTextField,
+              ),
+            ],
+          ),
+        ));
+
+    // The OK button
+
+    final okButton = ElevatedButton(
+      child: _createText('OK', fontSize: 12, weighted: false),
+      onPressed: () async {
+        await _saveKey(context, formKey, inputKey, keyVerified);
+      },
+    );
+
+    // The Cancel button
+
+    final cancelButton = ElevatedButton(
+      child: _createText('Cancel', fontSize: 12, weighted: false),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => widget.child),
+        );
+      },
+    );
+
     return Scaffold(
         body: Padding(
             padding: const EdgeInsets.all(32),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              FormBuilder(
-                key: formKey,
-                onChanged: () {
-                  formKey.currentState!.save();
-                },
-                autovalidateMode: AutovalidateMode.always,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Text(
-                      'Security Key',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    const Divider(color: Colors.grey),
-                    const SizedBox(height: 20),
-                    const Text(
-                      message,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    StatefulBuilder(
-                      builder: (BuildContext context, StateSetter setState) {
-                        return FormBuilderTextField(
-                          name: inputKey,
-                          obscureText:
-                              !_showKey, // Controls whether the security key is shown or hidden.
-                          autocorrect: false,
-                          decoration: InputDecoration(
-                            labelText: 'SECURITY KEY',
-                            labelStyle: const TextStyle(
-                              color: Colors.blue,
-                              letterSpacing: 1.5,
-                              fontSize: 13.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            suffixIcon: IconButton(
-                              icon: Icon(_showKey
-                                  ? Icons.visibility
-                                  : Icons.visibility_off),
-                              onPressed: () {
-                                setState(() {
-                                  // Toggle the state to show/hide the security key.
-                                  _showKey = !_showKey;
-                                });
-                              },
-                            ),
-                          ),
-                          validator: FormBuilderValidators.compose([
-                            FormBuilderValidators.required(),
-                            (val) {
-                              if (!widget
-                                  .verifySecurityKeyFunc(val as String)) {
-                                keyVerified = false;
-                                debugPrint('keyVerified: $keyVerified');
-                                return 'Incorrect Security Key';
-                              } else {
-                                keyVerified = true;
-                                debugPrint('keyVerified: $keyVerified');
-                              }
-                              return null;
-                            },
-                          ]),
-                        );
-                      },
-                    ),
-                  ],
-                ),
+              formBuilder,
+              smallGapV,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [okButton, smallGapH, cancelButton],
               ),
-              const SizedBox(height: 10),
-              Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                ElevatedButton(
-                    onPressed: () async {
-                      if (keyVerified) {
-                        debugPrint('keyVerified: $keyVerified');
-                        final formData = formKey.currentState?.value as Map;
-                        await KeyManager.setSecurityKey(
-                            formData[inputKey].toString());
-                        debugPrint('security key saved');
-                        Navigator.pop(context);
-                      } else {
-                        await showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: const Text('Incorrect Security Key'),
-                            content: const Text(
-                                'The security key entered is incorrect!'),
-                            actions: [
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('Dismiss'))
-                            ],
-                          ),
-                        );
-                      }
-                    },
-                    child: const Text(
-                      'OK',
-                      style: TextStyle(color: Colors.black, fontSize: 12),
-                    )),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => widget.child),
-                      );
-                    },
-                    child: const Text(
-                      'Cancel',
-                      style: TextStyle(color: Colors.black, fontSize: 12),
-                    ))
-              ]),
             ])));
   }
+
+  // Save the security key locally
+
+  Future<void> _saveKey(
+      BuildContext context,
+      GlobalKey<FormBuilderState> formKey,
+      String inputKey,
+      bool keyVerified) async {
+    if (keyVerified) {
+      debugPrint('keyVerified: $keyVerified');
+      final formData = formKey.currentState?.value as Map;
+      await KeyManager.setSecurityKey(formData[inputKey].toString());
+      debugPrint('Security key saved');
+      Navigator.pop(context);
+    } else {
+      await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Incorrect Security Key'),
+          content: const Text('The security key entered is incorrect!'),
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Dismiss'))
+          ],
+        ),
+      );
+    }
+  }
+}
+
+// Create a Text widget
+Text _createText(String str, {required double fontSize, bool weighted = true}) {
+  return Text(str,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: fontSize,
+        fontWeight: weighted ? FontWeight.w500 : null,
+      ));
 }
