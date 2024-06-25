01c431d4db948c0f6ab4100e29565b133f7447d5
cdawei <dawei.chen@anu.edu.au>
2024-06-25 23:17:19 +1000

    Add improved version to init ACL file

--- lib/src/solid/utils/rdf.dart
+++ lib/src/solid/utils/rdf.dart
@@ -31,8 +31,6 @@ library;
 import 'package:rdflib/rdflib.dart';
 import 'package:solidpod/src/solid/constants/common.dart';
 import 'package:solidpod/src/solid/constants/schema.dart';
-import 'package:solidpod/src/solid/constants/web_acl.dart';
-import 'package:solidpod/src/solid/utils/authdata_manager.dart';
 
 // /// Create and return a namespace
 // Namespace getNamespace(String ns) => Namespace(ns: ns);
@@ -66,7 +64,6 @@ String tripleMapToTurtle(Map<URIRef, Map<URIRef, dynamic>> triples,
 
   if (bindNamespaces != null) {
     bindNamespaces.forEach(g.bind);
-    print('To bind: ${[for (final ns in bindNamespaces.values) ns.ns]}');
   }
 
   g.serialize(abbr: 'short');
@@ -151,81 +148,12 @@ Map<String, dynamic> parseACL(String aclContent) {
   return dataMap;
 }
 
-// /// Generate TTL string for ACL file of a given resource
-// Future<String> genAclTTLStr(String resourceUrl,
-//     {AccessMode ownerAccess = AccessMode.control,
-//     AccessMode publicAccess = AccessMode.read}) async {
-//   final webId = await AuthDataManager.getWebId();
-//   assert(webId != null);
-
-//   final g = Graph();
-//   final f = URIRef(resourceUrl);
-//   final nsSub = Namespace(ns: '$resourceUrl.acl#');
-
-//   // URIRef(RESOURCE_URL.acl#owner):
-//   // 	       URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'): URIRef('http://www.w3.org/ns/auth/acl#Authorization'),
-//   //         URIRef('http://www.w3.org/ns/auth/acl#accessTo'): URIRef(RESOURCE_URL),
-//   //         URIRef('http://www.w3.org/ns/auth/acl#agent'): URIRef(WEB_ID),
-//   //         URIRef('http://www.w3.org/ns/auth/acl#mode'): URIRef('http://www.w3.org/ns/auth/acl#Control')},
-
-//   final ownerSub = nsSub.withAttr('owner');
-//   g.addTripleToGroups(
-//       ownerSub, rdfNS.ns.withAttr(typePred), aclNS.ns.withAttr(aclAuth));
-//   g.addTripleToGroups(ownerSub, aclNS.ns.withAttr(accessToPred), f);
-//   g.addTripleToGroups(ownerSub, aclNS.ns.withAttr(agentPred), URIRef(webId!));
-//   g.addTripleToGroups(ownerSub, aclNS.ns.withAttr(modePred),
-//       aclNS.ns.withAttr(ownerAccess.mode));
-
-//   // URIRef(RESOURCE_URL.acl#public'):
-//   //    URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'): URIRef('http://www.w3.org/ns/auth/acl#Authorization'),
-//   //    URIRef('http://www.w3.org/ns/auth/acl#accessTo'): URIRef(RESOURCE_URL),
-//   //    URIRef('http://www.w3.org/ns/auth/acl#agentClass'): URIRef('http://xmlns.com/foaf/0.1/Agent'),
-//   //    URIRef('http://www.w3.org/ns/auth/acl#mode'): URIRef('http://www.w3.org/ns/auth/acl#Read')
-
-//   final publicSub = nsSub.withAttr('public');
-//   g.addTripleToGroups(
-//       publicSub, rdfNS.ns.withAttr(typePred), aclNS.ns.withAttr(aclAuth));
-//   g.addTripleToGroups(publicSub, aclNS.ns.withAttr(accessToPred), f);
-//   g.addTripleToGroups(publicSub, aclNS.ns.withAttr(agentClassPred),
-//       foafNS.ns.withAttr(aclAgent));
-//   g.addTripleToGroups(publicSub, aclNS.ns.withAttr(modePred),
-//       aclNS.ns.withAttr(publicAccess.mode));
-
-//   // Bind the long namespace to shorter string for better readability
-
-//   g.bind(aclNS.prefix, aclNS.ns);
-//   // g.bind('foaf', nsFoaf); // causes "Exception: foaf: already exists in prefixed namespaces!"
-//   g.bind(rdfNS.prefix, rdfNS.ns);
-
-//   // Serialise to TTL string
-
-//   g.serialize(abbr: 'short');
-
-//   return g.serializedString;
-// }
-
 /// Generate permission log file content
-Future<String> genPermLogTTLStr(String resourceUrl) async {
-  final g = Graph();
-  final f = URIRef(resourceUrl);
-
-  // URIRef(RESOURCE_URL):
-  //     URIRef('http://purl.org/dc/terms/title'): Literal('Permissions Log'),
-  //     URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'): URIRef('http://xmlns.com/foaf/0.1/PersonalProfileDocument')
-
-  g.addTripleToGroups(f, termsNS.ns.withAttr(titlePred), logFileTitle);
-  g.addTripleToGroups(
-      f, rdfNS.ns.withAttr(typePred), foafNS.ns.withAttr(profileDoc));
-
-// Bind the long namespace to shorter string for better readability
-
-  g.bind(termsNS.prefix, termsNS.ns);
-  // g.bind('foaf', nsFoaf);
-  g.bind(rdfNS.prefix, rdfNS.ns);
-
-  // Serialise to TTL string
-
-  g.serialize(abbr: 'short');
-
-  return g.serializedString;
-}
+String genPermLogTTLStr(String resourceUrl) => tripleMapToTurtle({
+      URIRef(resourceUrl): {
+        termsNS.ns.withAttr(titlePred): logFileTitle,
+        rdfNS.ns.withAttr(typePred): foafNS.ns.withAttr(profileDoc),
+      }
+    }, bindNamespaces: {
+      termsNS.prefix: termsNS.ns
+    });
