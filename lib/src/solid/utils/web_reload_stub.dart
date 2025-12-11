1e15241f8d4a30814ce56cea5ca570b17cfa24f6
Williams <lambadajester50@gmail.com>
2025-12-11 20:37:38 +1100

    new

--- lib/src/solid/utils/web_reload_stub.dart
+++
@@ -1,8 +0,0 @@
-/// Stub implementation for non-web platforms.
-/// Does nothing on mobile/desktop platforms.
-
-library;
-
-void reloadPage() {
-  // No-op on non-web platforms
-}
--- lib/src/solid/utils/web_reload_web.dart
+++
@@ -1,9 +0,0 @@
-/// Web-specific implementation to reload the page.
-
-library;
-
-import 'package:web/web.dart' as web;
-
-void reloadPage() {
-  web.window.location.reload();
-}
