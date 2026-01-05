aef008782f9cf9d4ff49fe8f1706416f2f0c5d80
cdawei <>
2026-01-05 11:48:21 +1100

    Add helper functions for file IO

---
+++ lib/src/solid/utils/io_helper.dart
@@ -0,0 +1,61 @@
+/// Helper functions for reading and writing files in PODs.
+///
+/// Copyright (C) 2026, Software Innovation Institute, ANU.
+///
+/// Licensed under the MIT License (the "License").
+///
+/// License: https://choosealicense.com/licenses/mit/.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+///
+/// Authors: Dawei Chen
+
+library;
+
+import 'package:solidpod/src/solid/utils/misc.dart';
+
+final _protectedFiles = <String>{};
+
+Future<bool> isFileProtected(String fileUrl) async {
+  if (_protectedFiles.isEmpty) {
+    // TODO: dc 20260105 - double check if these are all the protected files
+
+    final fileUrls = [
+      for (final f in [
+        await getEncKeyPath(),
+        await getIndKeyPath(),
+        await getPubKeyPath(),
+        await getPubIndKeyPath(),
+        await getAuthUserIndKeyPath(),
+        await getSharedKeyFilePath(),
+        await getPermLogFilePath(),
+      ])
+        await getFileUrl(f),
+    ];
+
+    _protectedFiles.addAll(fileUrls);
+    _protectedFiles.addAll(fileUrls.map((f) => '$f.acl'));
+    _protectedFiles.addAll([
+      for (final d in await generateDefaultFolders())
+        '${await getDirUrl(d)}.acl',
+    ]);
+  }
+
+  return _protectedFiles.contains(fileUrl);
+}
