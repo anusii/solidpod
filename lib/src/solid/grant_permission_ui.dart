1fdbc96050b3bb948503ad6089fc324bd6b35f3e
Jess Moore <jessclaremoore@gmail.com>
2026-01-09 19:37:37 +1100

    linted

--- lib/src/solid/grant_permission_ui.dart
+++ lib/src/solid/grant_permission_ui.dart
@@ -71,10 +71,11 @@ class GrantPermissionUi extends StatefulWidget {
     this.onNavigateBack,
     super.key,
   }) : assert(
-            // Requires externalWebId of resource owner to be provided if resource
-            // is an externally owned resource.
-            isExternalRes == false || externalWebId != null,
-            'externalWebId must be provided if isExternalRes == true');
+          // Requires externalWebId of resource owner to be provided if resource
+          // is an externally owned resource.
+          isExternalRes == false || externalWebId != null,
+          'externalWebId must be provided if isExternalRes == true',
+        );
 
   /// The child widget to return to when back button is pressed and/or when
   /// page is reloaded after a permission is granted or revoked.
